465b8865228f4818059e8c4ab342eac219de6952
groms1989 <justgrom@yahoo.com>
2016-08-04 10:15:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'model/database.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <header>Groms NHL Stats</header>
+        <link rel="stylesheet" href="main.css" type="text/css">
+    </head>
+    <body>
+        <div>
+        <ul>
+            <li><a href="standings/leagueStandings.php">Standings</a></li>
+            <li><a href="skaterStats/stats.php">Skater Stats</a></li>
+            <li><a href="Info/anaheimInfo.php">Rosters</a></li>
+        </ul>
+    </div>
+    </body>
+</html>
+<?php include 'view/footer.php'; ?>
